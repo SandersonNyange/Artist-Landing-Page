c7ef5b9b2430eebce452cfa71fc578dcbd6afc41
Mwakamba <101863068+SandersonNyange@users.noreply.github.com>
2022-04-11 23:03:44 +0300

    Delete phpGuide/phpWalkThrough directory

--- phpGuide/phpWalkThrough/inc/footer.php
+++
@@ -1,5 +0,0 @@
-
-<script src="jquery-3.3.1.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
--- phpGuide/phpWalkThrough/inc/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Feedback</title>
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="inc/style.css">
-</head>
-<body>
-<?php include('navbar.php'); ?>
